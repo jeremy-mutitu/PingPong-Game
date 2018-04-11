7563eaa36a9481fb4709b52058da2cf9a26a89f4
jeremy-mutitu <jeremiahmutitu@gmail.com>
2018-04-11 09:55:10 +0300

    added calculation code for the business logic

--- js/business-logic.js
+++ js/business-logic.js
@@ -1,2 +1,12 @@
 var answer = "";
 var theResult = [];
+var condition = function (number) {
+  if (number % 15 === 0) {
+    answer = "PingPong";
+  } else if (number % 5 === 0) {
+    answer = "Pong";
+  } else if (number % 3 === 0) {
+    answer = "Ping";
+  } else answer = number;
+  return answer;
+}
